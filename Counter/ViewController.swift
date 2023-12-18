351dc780a6cce515b67ab691632c3aa0408e466d
Ilya Kalin <ilya.muromec888@gmail.com>
2023-12-18 16:45:04 +0300

    Базовая версия Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButtonPlus: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    private var counter: Int = 0
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+    }
+    
 }
 
